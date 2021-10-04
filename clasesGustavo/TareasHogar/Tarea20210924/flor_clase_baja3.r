fe09d4d7d0c1e168b973277a4cf955035e2ff014
florfederico <89236334+florfederico@users.noreply.github.com>
2021-10-03 21:00:58 -0300

    Update flor_clase_baja3.r

--- clasesGustavo/TareasHogar/Tarea20210924/flor_clase_baja3.r
+++ clasesGustavo/TareasHogar/Tarea20210924/flor_clase_baja3.r
@@ -21,6 +21,7 @@ gc()             #garbage collection
 require("data.table")
 require("rlist")
 require("yaml")
+require("dplyr")
 
 require("lightgbm")
 
@@ -29,16 +30,16 @@ require("DiceKriging")
 require("mlrMBO")
 
 #paquete para hacer queries sobre el dataset
-library(sqldf)
+#library(sqldf)
 
 
 #para poder usarlo en la PC y en la nube sin tener que cambiar la ruta
 #cambiar aqui las rutas en su maquina
 switch ( Sys.info()[['sysname']],
-         Windows = { directory.root  <-  "C:/Users/feder/OneDrive/Maestría Big Data/Laboratorio de Implementacion I/DM_EyF" },   #Windows
+         Windows = { directory.root  <-  "C:/Users/feder/OneDrive/Maestr?a Big Data/Laboratorio de Implementacion I/DM_EyF" },   #Windows
          Darwin  = { directory.root  <-  "~/dm/" },  #Apple MAC
          Linux   = { directory.root  <-  "~/buckets/b1/" } #Google Cloud
-       )
+)
 #defino la carpeta donde trabajo
 setwd( directory.root )
 
@@ -48,25 +49,25 @@ kexperimento  <- NA   #NA si se corre la primera vez, un valor concreto si es pa
 
 kscript         <- "761_lgb_bin2_apiacere"
 
-karch_dataset    <- "./datasets/datasets_paquete_premium_corregido_ext.csv.gz"
+karch_dataset    <- "./datasets/paquete_premium_baja3.csv.gz"
 kmes_apply       <- 202101  #El mes donde debo aplicar el modelo
 kmes_train_hasta <- 202011  #Obvimente, solo puedo entrenar hasta 202011
 
 #kmes_train_desde <- 202001  #Entreno desde Enero-2020
 
-kmes_train_desde <- 202003  #Elijo entrenar desde que empezó la pandemia
+kmes_train_desde <- 202003  #Elijo entrenar desde que empezo la pandemia
 
 kcanaritos  <-  100
 kBO_iter    <-  100   #cantidad de iteraciones de la Optimizacion Bayesiana
 
 #Aqui se cargan los hiperparametros
 hs <- makeParamSet( 
-         makeNumericParam("learning_rate",    lower=    0.02 , upper=    0.06),
-         makeNumericParam("feature_fraction", lower=    0.1  , upper=    0.4),
-         makeIntegerParam("min_data_in_leaf", lower= 1000L   , upper= 8000L),
-         makeIntegerParam("num_leaves",       lower=  100L   , upper= 1024L),
-         makeNumericParam("prob_corte",       lower=    0.040, upper=    0.055)
-        )
+  makeNumericParam("learning_rate",    lower=    0.02 , upper=    0.06),
+  makeNumericParam("feature_fraction", lower=    0.1  , upper=    0.4),
+  makeIntegerParam("min_data_in_leaf", lower= 1000L   , upper= 8000L),
+  makeIntegerParam("num_leaves",       lower=  100L   , upper= 1024L),
+  makeNumericParam("prob_corte",       lower=    0.040, upper=    0.055)
+)
 
 campos_malos  <- c( "mpasivos_margen", "mactivos_margen", "mrentabilidad_annual", "mrentabilidad", "mcomisiones" )   #aqui se deben cargar todos los campos culpables del Data Drifting
 
@@ -78,15 +79,15 @@ ksemilla_azar  <- 102191  #Aqui poner la propia semilla
 get_experimento  <- function()
 {
   if( !file.exists( "./maestro.yaml" ) )  cat( file="./maestro.yaml", "experimento: 1000" )
-
+  
   exp  <- read_yaml( "./maestro.yaml" )
   experimento_actual  <- exp$experimento
-
+  
   exp$experimento  <- as.integer(exp$experimento + 1)
   Sys.chmod( "./maestro.yaml", mode = "0644", use_umask = TRUE)
   write_yaml( exp, "./maestro.yaml" )
   Sys.chmod( "./maestro.yaml", mode = "0444", use_umask = TRUE) #dejo el archivo readonly
-
+  
   return( experimento_actual )
 }
 #------------------------------------------------------------------------------
@@ -97,35 +98,35 @@ loguear  <- function( reg, arch=NA, folder="./work/", ext=".txt", verbose=TRUE )
 {
   archivo  <- arch
   if( is.na(arch) )  archivo  <- paste0(  folder, substitute( reg), ext )
-
+  
   if( !file.exists( archivo ) )  #Escribo los titulos
   {
     linea  <- paste0( "fecha\t", 
                       paste( list.names(reg), collapse="\t" ), "\n" )
-
+    
     cat( linea, file=archivo )
   }
-
+  
   linea  <- paste0( format(Sys.time(), "%Y%m%d %H%M%S"),  "\t",     #la fecha y hora
                     gsub( ", ", "\t", toString( reg ) ),  "\n" )
-
+  
   cat( linea, file=archivo, append=TRUE )  #grabo al archivo
-
+  
   if( verbose )  cat( linea )   #imprimo por pantalla
 }
 #------------------------------------------------------------------------------
 
-PROB_CORTE  <- 0.030
+PROB_CORTE  <- 0.045
 
 fganancia_logistic_lightgbm   <- function(probs, datos) 
 {
   vlabels  <- getinfo(datos, "label")
   vpesos   <- getinfo(datos, "weight")
-
+  
   #aqui esta el inmoral uso de los pesos para calcular la ganancia correcta
   gan  <- sum( (probs > PROB_CORTE  ) *
-               ifelse( vlabels== 1 & vpesos > 1, 48750, -1250 ) )
-
+                 ifelse( vlabels== 1 & vpesos > 1, 48750, -1250 ) )
+  
   return( list( "name"= "ganancia", 
                 "value"=  gan,
                 "higher_better"= TRUE ) )
@@ -137,12 +138,12 @@ fganancia_logistic_lightgbm   <- function(probs, datos)
 EstimarGanancia_lightgbm  <- function( x )
 {
   GLOBAL_iteracion  <<- GLOBAL_iteracion + 1
-
+  
   gc()
   PROB_CORTE <<- x$prob_corte   #asigno la variable global
-
+  
   kfolds  <- 5   # cantidad de folds para cross validation
-
+  
   param_basicos  <- list( objective= "binary",
                           metric= "custom",
                           first_metric_only= TRUE,
@@ -157,13 +158,13 @@ EstimarGanancia_lightgbm  <- function( x )
                           max_bin= 31,            #por ahora, lo dejo fijo
                           num_iterations= 9999,    #un numero muy grande, lo limita early_stopping_rounds
                           force_row_wise= TRUE    #para que los alumnos no se atemoricen con tantos warning
-                        )
-
+  )
+  
   #el parametro discolo, que depende de otro
   param_variable  <- list(  early_stopping_rounds= as.integer(50 + 1/x$learning_rate) )
-
+  
   param_completo  <- c( param_basicos, param_variable, x )
-
+  
   set.seed( 999983 )
   modelocv  <- lgb.cv( data= dtrain,
                        eval= fganancia_logistic_lightgbm,
@@ -171,55 +172,55 @@ EstimarGanancia_lightgbm  <- function( x )
                        nfold= kfolds,    #folds del cross validation
                        param= param_completo,
                        verbose= -100
-                      )
-
-
+  )
+  
+  
   ganancia  <- unlist(modelocv$record_evals$valid$ganancia$eval)[ modelocv$best_iter ]
-
+  
   ganancia_normalizada  <-  ganancia* kfolds  
   attr(ganancia_normalizada ,"extras" )  <- list("num_iterations"= modelocv$best_iter)  #esta es la forma de devolver un parametro extra
-
+  
   param_completo$num_iterations <- modelocv$best_iter  #asigno el mejor num_iterations
   param_completo["early_stopping_rounds"]  <- NULL
-
-   #si tengo una ganancia superadora, genero el archivo para Kaggle
-   if(  ganancia > GLOBAL_ganancia_max )
-   {
-     GLOBAL_ganancia_max  <<- ganancia  #asigno la nueva maxima ganancia a una variable GLOBAL, por eso el <<-
-
-     set.seed(ksemilla_azar)
-
-     modelo  <- lightgbm( data= dtrain,
-                          param= param_completo,
-                          verbose= -100
-                        )
-
+  
+  #si tengo una ganancia superadora, genero el archivo para Kaggle
+  if(  ganancia > GLOBAL_ganancia_max )
+  {
+    GLOBAL_ganancia_max  <<- ganancia  #asigno la nueva maxima ganancia a una variable GLOBAL, por eso el <<-
+    
+    set.seed(ksemilla_azar)
+    
+    modelo  <- lightgbm( data= dtrain,
+                         param= param_completo,
+                         verbose= -100
+    )
+    
     #calculo la importancia de variables
     tb_importancia  <- lgb.importance( model= modelo )
     fwrite( tb_importancia, 
             file= paste0(kimp, "imp_", GLOBAL_iteracion, ".txt"),
             sep="\t" )
-
-     prediccion  <- predict( modelo, data.matrix( dapply[  , campos_buenos, with=FALSE]) )
-
-     Predicted  <- as.integer( prediccion > x$prob_corte )
-
-     entrega  <- as.data.table( list( "numero_de_cliente"= dapply$numero_de_cliente, 
-                                      "Predicted"= Predicted)  )
-
-     #genero el archivo para Kaggle
-     fwrite( entrega, 
-             file= paste0(kkaggle, GLOBAL_iteracion, ".csv" ),
-             sep= "," )
-   }
-
-   #logueo 
-   xx  <- param_completo
-   xx$iteracion_bayesiana  <- GLOBAL_iteracion
-   xx$ganancia  <- ganancia_normalizada   #le agrego la ganancia
-   loguear( xx,  arch= klog )
-
-   return( ganancia )
+    
+    prediccion  <- predict( modelo, data.matrix( dapply[  , campos_buenos, with=FALSE]) )
+    
+    Predicted  <- as.integer( prediccion > x$prob_corte )
+    
+    entrega  <- as.data.table( list( "numero_de_cliente"= dapply$numero_de_cliente, 
+                                     "Predicted"= Predicted)  )
+    
+    #genero el archivo para Kaggle
+    fwrite( entrega, 
+            file= paste0(kkaggle, GLOBAL_iteracion, ".csv" ),
+            sep= "," )
+  }
+  
+  #logueo 
+  xx  <- param_completo
+  xx$iteracion_bayesiana  <- GLOBAL_iteracion
+  xx$ganancia  <- ganancia_normalizada   #le agrego la ganancia
+  loguear( xx,  arch= klog )
+  
+  return( ganancia )
 }
 #------------------------------------------------------------------------------
 #Aqui empieza el programa
@@ -240,99 +241,58 @@ GLOBAL_iteracion  <- 0
 #si ya existe el archivo log, traigo hasta donde llegue
 if( file.exists(klog) )
 {
- tabla_log  <- fread( klog)
- GLOBAL_iteracion  <- nrow( tabla_log ) -1
- GLOBAL_ganancia_max  <- tabla_log[ , max(ganancia) ]
+  tabla_log  <- fread( klog)
+  GLOBAL_iteracion  <- nrow( tabla_log ) -1
+  GLOBAL_ganancia_max  <- tabla_log[ , max(ganancia) ]
 }
 
 
 #cargo el dataset que tiene los 36 meses
 dataset  <- fread(karch_dataset)
 
-#genero la BAJA+3
-dataset_b3 <- sqldf( "
-SELECT W.*,
-	CASE
-					WHEN B3.CLASE IS NULL THEN W.CLASE_TERNARIA
-					ELSE B3.CLASE
-	END AS CLASE_TERNARIA_NUEVA
-FROM
-	(SELECT *
-		FROM dataset
-		WHERE 1 = 1
-			AND FOTO_MES >= 202003
-			AND FOTO_MES < 202012) W
-LEFT JOIN
-	(SELECT X.NUMERO_DE_CLIENTE,
-			X.FOTO_MES,
-			'BAJA+3' CLASE
-		FROM dataset X
-		WHERE 1 = 1
-			AND X.FOTO_MES >= 202003
-			AND X.FOTO_MES < 202011
-			AND X.CLASE_TERNARIA = 'CONTINUA'
-			AND NOT EXISTS
-				(SELECT *
-					FROM dataset Y
-					WHERE 1 = 1
-						AND Y.FOTO_MES >= 202003
-						AND Y.FOTO_MES < 202011
-						AND Y.FOTO_MES = X.FOTO_MES + 2
-						AND Y.NUMERO_DE_CLIENTE = X.NUMERO_DE_CLIENTE ) ) B3 ON W.NUMERO_DE_CLIENTE = B3.NUMERO_DE_CLIENTE
-AND W.FOTO_MES = B3.FOTO_MES
-ORDER BY W.FOTO_MES,
-	W.NUMERO_DE_CLIENTE
-	")
-
-#elimino el dataset para liberar memoria RAM
-rm( dataset )
-gc()
-
-campos_lags  <- setdiff(  colnames(dataset3) ,  c("clase_ternaria","clase01", "numero_de_cliente","foto_mes", campos_malos) )
+campos_lags  <- setdiff(  colnames(dataset) ,  c("clase_ternaria","clase01", "numero_de_cliente","foto_mes", campos_malos) )
 
 #Hago feature Engineering en este mismo script
 #agreglo los lags de orden 1
-setorderv( dataset3, c("numero_de_cliente","foto_mes") )
-dataset3[ , paste0( campos_lags, "_lag1") := shift(.SD, 1, NA, "lag"), 
-           by= numero_de_cliente, 
-           .SDcols= campos_lags]
+setorderv( dataset, c("numero_de_cliente","foto_mes") )
+dataset[ , paste0( campos_lags, "_lag1") := shift(.SD, 1, NA, "lag"), 
+         by= numero_de_cliente, 
+         .SDcols= campos_lags]
 
 #agrego los deltas de los lags, con un "for" nada elegante
 for( vcol in campos_lags )
 {
-   dataset3[,  paste0(vcol, "_delta1") := get( vcol)  - get(paste0( vcol, "_lag1"))]
+  dataset[,  paste0(vcol, "_delta1") := get( vcol)  - get(paste0( vcol, "_lag1"))]
 }
 
 
 #agrego canaritos
 if( kcanaritos > 0 )
 {
-  for( i  in 1:kcanaritos)  dataset3[ , paste0("canarito", i ) :=  runif( nrow(dataset3))]
+  for( i  in 1:kcanaritos)  dataset[ , paste0("canarito", i ) :=  runif( nrow(dataset))]
 }
 
 
 #cargo los datos donde voy a aplicar el modelo
-dapply  <- copy( dataset3[  foto_mes==kmes_apply ] )
-
+dapply  <- copy( dataset[  foto_mes==kmes_apply ] )
+#dapply <- karch_aplicacion
 
 #creo la clase_binaria2   1={ BAJA+2,BAJA+1}  0={CONTINUA}
-dataset3[ , clase01:= ifelse( clase_ternaria=="CONTINUA", 0, 1 ) ]
-
-
+dataset[ , clase01:= ifelse( clase_ternaria=="CONTINUA", 0, 1 ) ]
 
 #los campos que se van a utilizar
-campos_buenos  <- setdiff( colnames(dataset3), c("clase_ternaria","clase01", campos_malos) )
+campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01", campos_malos) )
 
 #dejo los datos en el formato que necesita LightGBM
 #uso el weight como un truco ESPANTOSO para saber la clase real
-dtrain  <- lgb.Dataset( data= data.matrix(  dataset3[  foto_mes>=kmes_train_desde & foto_mes<=kmes_train_hasta , campos_buenos, with=FALSE]),
-                        label=  dataset3[ foto_mes>=kmes_train_desde & foto_mes<=kmes_train_hasta, clase01],
-                        weight=  dataset3[ foto_mes>=kmes_train_desde & foto_mes<=kmes_train_hasta , ifelse(clase_ternaria=="BAJA+2", 1.0000001, 1.0)] ,
+dtrain  <- lgb.Dataset( data= data.matrix(  dataset[  foto_mes>=kmes_train_desde & foto_mes<=kmes_train_hasta , campos_buenos, with=FALSE]),
+                        label=  dataset[ foto_mes>=kmes_train_desde & foto_mes<=kmes_train_hasta, clase01],
+                        weight=  dataset[ foto_mes>=kmes_train_desde & foto_mes<=kmes_train_hasta , ifelse(clase_ternaria=="BAJA+2", 1.0000001, 1.0)] ,
                         free_raw_data= TRUE
-                      )
+)
 
 #elimino el dataset para liberar memoria RAM
-rm( dataset3 )
+rm( dataset )
 gc()
 
 #Aqui comienza la configuracion de la Bayesian Optimization
@@ -344,12 +304,12 @@ configureMlr( show.learner.output= FALSE)
 #configuro la busqueda bayesiana,  los hiperparametros que se van a optimizar
 #por favor, no desesperarse por lo complejo
 obj.fun  <- makeSingleObjectiveFunction(
-              fn=       funcion_optimizar, #la funcion que voy a maximizar
-              minimize= FALSE,   #estoy Maximizando la ganancia
-              noisy=    TRUE,
-              par.set=  hs,     #definido al comienzo del programa
-              has.simple.signature = FALSE   #paso los parametros en una lista
-             )
+  fn=       funcion_optimizar, #la funcion que voy a maximizar
+  minimize= FALSE,   #estoy Maximizando la ganancia
+  noisy=    TRUE,
+  par.set=  hs,     #definido al comienzo del programa
+  has.simple.signature = FALSE   #paso los parametros en una lista
+)
 
 ctrl  <- makeMBOControl( save.on.disk.at.time= 600,  save.file.path= kbayesiana)  #se graba cada 600 segundos
 ctrl  <- setMBOControlTermination(ctrl, iters= kBO_iter )   #cantidad de iteraciones
